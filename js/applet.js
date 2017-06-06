9cb293a1489c21934ba1588b5b010052159da6ef
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-06-06 00:10:28 -0400

    Implemented a simple tank object

--- js/applet.js
+++ js/applet.js
@@ -1,6 +1,31 @@
 var sketch = new p5(function(p) {
+	function Tank() {
+		// top-left corner of tank
+		this.pos = p.createVector(30, p.height / 2);
+		this.width = p.width - 60;
+		this.height = p.height / 2 - 10;
+
+		this.draw = function() {
+			p.fill(p.color(0, 140, 184, 128));
+			p.beginShape();
+			p.vertex(this.pos.x, this.pos.y);
+			p.vertex(this.pos.x, this.pos.y + this.height);
+			p.vertex(this.pos.x + this.width, this.pos.y + this.height);
+			p.vertex(this.pos.x + this.width, this.pos.y);
+			p.endShape();
+		}
+	}
+
+	var tank;
+
 	p.setup = function() {
 		var cnv = p.createCanvas(800, 600);
-		p.background(0);
+		tank = new Tank();
+	}
+
+	p.draw = function() {
+		p.background('#bef5ff');
+
+		tank.draw(); // draw tank over any other objects b/c it is translucent
 	}
 }, 'sim');
